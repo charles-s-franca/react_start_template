9dccb620b3f773015e3e27ed0e37a5218e9df00d
Charles Franca <charlesfranca@Charless-Mac-mini.local>
2018-02-27 16:26:40 -0300

    implantação da navegação de páginas

---
+++ src/components/pages/Forum.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
+class Forum extends Component {
+  render() {
+
+    return (
+      <div>
+        Nosso Forum
+      </div>
+    );
+  }
+}
+
+export default Forum;
